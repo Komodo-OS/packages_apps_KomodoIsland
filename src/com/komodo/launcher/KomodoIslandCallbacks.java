8d761a0eb1abfa8fbe49e6aa056949d8a251d3ea
Edward Cross <edward.k.cross@gmail.com>
2019-10-06 18:50:01 -0400

    KomodoLauncher: Google Feed integration
    If Google Search is installed, the Google Feed can be found at
    the -1 screen (which is the very left).
    This feature is enabled by default.
    
    Change-Id: I495196818699fd378cd60e6dd61b07a0ab951762

--- src/com/komodo/launcher/KomodoIslandCallbacks.java
+++ src/com/komodo/launcher/KomodoIslandCallbacks.java
@@ -22,6 +22,10 @@ import android.os.Bundle;
 
 import com.android.launcher3.AppInfo;
 import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.settings.SettingsActivity;
+import com.android.launcher3.Utilities;
+
+import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -29,9 +33,16 @@ import java.util.ArrayList;
 
 public class KomodoIslandCallbacks implements LauncherCallbacks,
         SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     private final KomodoIsland mLauncher;
 
+    private OverlayCallbackImpl mOverlayCallbacks;
+    private LauncherClient mLauncherClient;
+
+    private boolean mStarted;
+    private boolean mResumed;
+    private boolean mAlreadyOnHome;
 
     public KomodoIslandCallbacks(KomodoIsland launcher) {
         mLauncher = launcher;
@@ -39,32 +50,50 @@ public class KomodoIslandCallbacks implements LauncherCallbacks,
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        SharedPreferences prefs = Utilities.getPrefs(mLauncher);
+        mOverlayCallbacks = new OverlayCallbackImpl(mLauncher);
+        mLauncherClient = new LauncherClient(mLauncher, mOverlayCallbacks, getClientOptions(prefs));
+        mOverlayCallbacks.setClient(mLauncherClient);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
+        mResumed = true;
+        if (mStarted) {
+            mAlreadyOnHome = true;
+        }
 
+        mLauncherClient.onResume();
     }
 
     @Override
     public void onStart() {
-
+        mStarted = true;
+        mLauncherClient.onStart();
     }
 
     @Override
     public void onStop() {
+        mStarted = false;
+        if (!mResumed) {
+            mAlreadyOnHome = false;
+        }
 
+        mLauncherClient.onStop();
     }
 
     @Override
     public void onPause() {
-
+        mResumed = false;
+        mLauncherClient.onPause();
     }
 
     @Override
     public void onDestroy() {
+        mLauncherClient.onDestroy();
 
+        Utilities.getPrefs(mLauncher).unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -84,12 +113,12 @@ public class KomodoIslandCallbacks implements LauncherCallbacks,
 
     @Override
     public void onAttachedToWindow() {
-
+        mLauncherClient.onAttachedToWindow();
     }
 
     @Override
     public void onDetachedFromWindow() {
-
+        mLauncherClient.onDetachedFromWindow();
     }
 
     @Override
@@ -99,7 +128,7 @@ public class KomodoIslandCallbacks implements LauncherCallbacks,
 
     @Override
     public void onHomeIntent(boolean internalStateHandled) {
-
+        mLauncherClient.hideOverlay(mAlreadyOnHome);
     }
 
     @Override
@@ -124,6 +153,17 @@ public class KomodoIslandCallbacks implements LauncherCallbacks,
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (SettingsActivity.MINUS_ONE_KEY.equals(key)) {
+            mLauncherClient.setClientOptions(getClientOptions(sharedPreferences));
+        }
+    }
 
+    private LauncherClient.ClientOptions getClientOptions(SharedPreferences prefs) {
+        boolean hasPackage = ParanoidUtils.hasPackageInstalled(mLauncher, SEARCH_PACKAGE);
+        boolean isEnabled = prefs.getBoolean(SettingsActivity.MINUS_ONE_KEY, true);
+        return new LauncherClient.ClientOptions(hasPackage && isEnabled,
+                true, /* enableHotword */
+                true /* enablePrewarming */
+        );
     }
 }
